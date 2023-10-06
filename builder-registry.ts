c0877eca9b54f0f179b26352cd9f315e9852e1ad
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-10-06 23:16:25 +0700

    Add team grid

--- builder-registry.ts
+++ builder-registry.ts
@@ -3,6 +3,7 @@ import { Builder } from "@builder.io/react";
 import Counter from "./components/Counter/Counter";
 import Hero from "./components/Hero/Hero";
 import Features from "./components/Features/Features";
+import TeamGrid from "./components/Team/TeamGrid";
 
 Builder.registerComponent(Counter, {
   name: "Counter",
@@ -46,4 +47,8 @@ Builder.registerComponent(Features, {
       defaultValue: "Designed for business teams like yours",
     },
   ],
+});
+
+Builder.registerComponent(TeamGrid, {
+  name: "TeamGrid",
 });
